74f3dd85b4ba07dc6fee3f96df712921cad706ef
addyd <draberadrian@gmail.com>
2023-07-01 19:44:01 +0100

    Prototype 2
    Need to add culling

--- prototype2.py
+++ prototype2.py
@@ -19,8 +19,9 @@ cubeEdges = ((0,1),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(
 #Finally, connect the vertices via their number to also make a cube.
 #It helps to draw a diagram at this point. The page I'm following here has this diagram:
 # https://stackabuse.s3.amazonaws.com/media/advanced-opengl-in-python-pygame-and-pyopengl-2.png
-cubeQuads = ((0,3,6,4),(2,5,6,3),(1,2,5,7),(1,0,4,7),(7,4,6,5),(2,3,0,1))
-
+cubeQuads = ((3,2,1,0),(0,1,7,4),(3,2,5,6),(3,0,4,6),(2,1,7,5),(4,7,5,6))
+colours = ((0,0,1),(0,1,0),(1,0,0),(0,1,1),(1,0,1),(1,1,0),(0.5,0.5,0.5),(1,1,1))
+#pygame
 # draw wire cube
 def wireCube():
     glBegin(GL_LINES)  # begin drawing. GL_LINES tells us we'll be drawing lines
@@ -39,16 +40,23 @@ def wireCube():
 
 #following similar logic:
 def solidCube():
+    x=0
     glBegin(GL_QUADS)
     for cubeQuad in cubeQuads:
+        x+=1
         for cubeVertex in cubeQuad:
+            glColor3fv(colours[x])
             glVertex3fv(cubeVertices[cubeVertex])
     glEnd()
 
 def main():
     pg.init()
+
     display = (1680, 1050)
     pg.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    #glEnable(GL_CULL_FACE)
+    #glCullFace(GL_FRONT)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -5)
@@ -62,15 +70,26 @@ def main():
         # Transformation matrix is T1 before this block of code
         #use an inbuilt openGL stack, allowing us to render multiple things:
         #glPushMatrix()
+        keys=pg.key.get_pressed()
 
         #Matrices are done in REVERSE
-        glRotatef(1, 1, 0, 1)
-        wireCube()
+        if keys[K_w]:
+            glRotatef(1, 0, 0, 1) # angle, x, y, z
+        if keys[K_e]:
+            glRotatef(-1, 0, 0, 1)
+        if keys[K_s]:
+            glRotatef(-1, 0, 1, 0)
+        if keys[K_d]:
+            glRotatef(1, 0, 1, 0)
+        if keys[K_q]:
+            glRotatef(-1, 1, 0, 0)
+        if keys[K_a]:
+            glRotatef(1, 1, 0, 0)
         #glPopMatrix()
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        #solidCube()
-        wireCube()
+        solidCube()
+        #wireCube()
         pg.display.flip() #update window with active buffer contents
         pg.time.wait(10)
 
