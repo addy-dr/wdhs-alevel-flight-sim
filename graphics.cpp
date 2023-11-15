21f6225d2d6c0f3feb02b931614bebd9b9d45e40
+D <112191393+addy-dr@users.noreply.github.com>
2023-11-15 19:40:25 +0000

    Started work on implementing c++ graphics processing

---
+++ graphics.cpp
@@ -0,0 +1,30 @@
+#include <pybind11/pybind11.h>
+#include <GL/glew.h>
+
+void initializeOpenGL() {
+    // Initialize OpenGL here
+    // Set up shaders, buffers, etc.
+}
+
+void renderTriangle(float t1[3], float t2[3], float t3[3], float c[3]) {
+    glBegin(GL_TRIANGLES);
+    glColor3fv(c);
+    glVertex3fv(t1);
+    glVertex3fv(t2);
+    glVertex3fv(t3);
+    glEnd();
+}
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(OpenGLWrapper, m) {
+    m.doc() = "Renders the environment and world of the flight simulator";
+
+    m.def("initializeOpenGL", &initializeOpenGL, "Initialises opengl");
+    m.def("renderTriangle", &renderTriangle, "Renders the individual triangles of the terrain");
+    m.def("add", &renderTriangle, "adds");
+}
