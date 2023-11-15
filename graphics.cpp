e7dbe4a5c6c6ad22123b65884d9743788a6ece40
+D <112191393+addy-dr@users.noreply.github.com>
2023-11-15 19:45:19 +0000

    Tested and implemented an add function via cpp

--- graphics.cpp
+++ graphics.cpp
@@ -7,12 +7,12 @@ void initializeOpenGL() {
 }
 
 void renderTriangle(float t1[3], float t2[3], float t3[3], float c[3]) {
-    glBegin(GL_TRIANGLES);
-    glColor3fv(c);
-    glVertex3fv(t1);
-    glVertex3fv(t2);
-    glVertex3fv(t3);
-    glEnd();
+    //glBegin(GL_TRIANGLES);
+    //glColor3fv(c);
+    //glVertex3fv(t1);
+    //glVertex3fv(t2);
+    //glVertex3fv(t3);
+    //glEnd();
 }
 
 int add(int i, int j) {
@@ -21,10 +21,10 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(OpenGLWrapper, m) {
+PYBIND11_MODULE(graphics, m) {
     m.doc() = "Renders the environment and world of the flight simulator";
 
     m.def("initializeOpenGL", &initializeOpenGL, "Initialises opengl");
     m.def("renderTriangle", &renderTriangle, "Renders the individual triangles of the terrain");
-    m.def("add", &renderTriangle, "adds");
+    m.def("add", &add, "adds");
 }
