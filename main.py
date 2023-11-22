d98f9bb9071f10490a51b03bc818ccd874e1ff6f
+D <112191393+addy-dr@users.noreply.github.com>
2023-11-22 19:06:09 +0000

    cleaned code up

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ print("Packages successfully loaded.")
 heightmap = np.array(Image.open('heightmap.bmp'))
 colourmap = np.array(Image.open('colourmap.bmp'))
 watermask = np.array(Image.open('watermask.bmp'))
-with open("NACA2412.json", "r+") as f:
+with open("NACA2412.json", "r+") as f: #produced using the Xfoil program from .dat files freely available at http://airfoiltools.com/airfoil/details?airfoil=naca2412-il
     naca2412_airfoil = json.load(f)[1]
 #map size
 ZLENGTH = len(heightmap)
@@ -31,7 +31,6 @@ XLENGTH = len(heightmap[0])
 RENDER_DISTANCE = 10
 
 class Camera:
-
     up = (0,1,0) #global definition of up independent of the camera
 
     def __init__(self, position, speed):
@@ -50,7 +49,7 @@ class Camera:
         return [self.__position[0], self.__position[2]]
 
     def getDir (self):
-        return self.__front
+        return self.__eulerAngles
 
     def update(self, keys, mouse):
         #returns a camera position vector. Otherwise, handles the lookat system and camera movement
@@ -96,9 +95,8 @@ class Camera:
                 newPos += ((self.__position[i]-self.__right[i]*speed),)
             self.__position = newPos
 
-        #use stars to unpack
         glLoadIdentity() #as per https://stackoverflow.com/questions/54316746/using-glulookat-causes-the-objects-to-spin
-        gluLookAt(*self.__position, *operateTuple(self.__position, self.__front, '+'), *self.__up)
+        gluLookAt(*self.__position, *operateTuple(self.__position, self.__front, '+'), *self.__up) #use stars to unpack
 
     def resolveForces(self, Thrust, deltaTime, Area = 16.2, mass = 1100):
         pass
@@ -129,18 +127,18 @@ def mapGen(heightmap, colourmap, watermask):
             if watermask[zcord][xcord][0] != 0: # => water tile as defined in the mask
                 vertList.append((xcord/3,0.75,zcord/3)) #so render as an ocean tile
                 coloursList.append((0.56+uniform(-0.05,0.05),0.72+uniform(-0.05,0.05),0.48+uniform(-0.05,0.05))) #generic lowlying land colour. Already check in map generating function if a tile is at sea level, so this is simply the colour for terrain sloping into sea level.
-                coloursList.append((0.56+uniform(-0.05,0.05),0.72+uniform(-0.05,0.05),0.48+uniform(-0.05,0.05))) #We do this twice since each "pixel" corresponds to two colours.
+                coloursList.append((0.56+uniform(-0.05,0.05),0.72+uniform(-0.05,0.05),0.48+uniform(-0.05,0.05))) #We do this twice since each "pixel" corresponds to two polygons.
             else:
                 vertList.append((xcord/3,heightmap[zcord][xcord]/150,zcord/3))
                 pixelColour = ((colourmap[zcord][xcord][0]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][1]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][2]/255)+uniform(-0.05,0.05)) #Convert from 0-255 RGB format to 0-1 RGB format. Random number adds colour variation for aesthetic purposes
                 coloursList.append(pixelColour) #define RGB values for all corresponding vertices
-
-                pixelColour = ((colourmap[zcord][xcord][0]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][1]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][2]/255)+uniform(-0.05,0.05)) #We do this twice since each "pixel" corresponds to two colours.
+                #We do this twice since each "pixel" corresponds to two polygons. We can afford to take more processing while loading the map at this stage.
+                pixelColour = ((colourmap[zcord][xcord][0]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][1]/255)+uniform(-0.05,0.05), (colourmap[zcord][xcord][2]/255)+uniform(-0.05,0.05))
                 coloursList.append(pixelColour)
     return np.array(vertList), np.array(coloursList)
 
 #renders a triangle based on the coords inputted
-def renderTriangle(t1, t2, t3, c):  #triangle 1, triangle 2, triangle 3, ocean tile? (not relevant here), colour
+def renderTriangle(t1, t2, t3, c):  #triangle 1, triangle 2, triangle 3, colour
     glBegin(GL_TRIANGLES)
     glColor3fv(c)
     glVertex3fv(t1)
@@ -156,16 +154,14 @@ def genTerrain(mapMatrix, coloursList, camPositionx, camPositionz):
     length = len(mapMatrix)
     try:
         for i in range(length):
-            #This stops vertices at the edge from rendering triangles - this previously led to triangles being rendered across the entire map
-            if i%XLENGTH == XLENGTH-1:
+            if ((camPositionx-mapMatrix[i][0])**2 + (camPositionz-mapMatrix[i][2])**2)**(1/2) > RENDER_DISTANCE: #only renders triangles within the render distance
                 pass
-            elif i+XLENGTH+1 > length: #ditto but for the other edge
+            elif i+XLENGTH+1 > length: #This stops vertices at the edge from rendering triangles - this previously led to triangles being rendered across the entire map
                 pass
-            elif ((camPositionx-mapMatrix[i][0])**2 + (camPositionz-mapMatrix[i][2])**2)**(1/2) > RENDER_DISTANCE:
+            elif i%XLENGTH == XLENGTH-1: #same as above but for other edge
                 pass
             else:
                 #the two triangles adjacent to any vertex
-                print()
                 if mapMatrix[i][1] == mapMatrix[i+1][1] == mapMatrix[i+XLENGTH][1] == 0.75: #This is only true if all three corners are at sea level
                     verticelist.append((mapMatrix[i+1],mapMatrix[i],mapMatrix[i+XLENGTH],coloursList[0]))
                 else:
@@ -190,13 +186,12 @@ def main():
     glutInit()
     glutInitDisplayMode(GLUT_RGBA)
     #pg.mouse.set_visible(False)
-    my_font = pg.font.Font('freesansbold.ttf', 32)
 
     display = (1920, 1080)
+    screen = pg.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     mainCam = Camera((30,3,60), 0.5) #position, speed (speed is a placeholder variable)
-
-    screen = pg.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glClearColor(25/255, 235/225, 235/225, 0) #sets the colour of the "sky"
 
     glMatrixMode(GL_PROJECTION)
     gluPerspective(60, (display[0]/display[1]), 0.1, 50.0) #fov, aspect, zNear, zFar
@@ -206,10 +201,11 @@ def main():
 
     mapMatrix, coloursList = mapGen(heightmap, colourmap, watermask)
     print("Map Generated")    #test
+
     genTerrain(mapMatrix, coloursList, *mainCam.getXZ()) #this first render is for debugging purposes
     print("Map Rendered")    #test
 
-    #culling
+    #culling settings
     glDepthMask(GL_TRUE)
     glDepthFunc(GL_LESS)
     glEnable(GL_DEPTH_TEST)
@@ -227,7 +223,7 @@ def main():
                 pg.quit()
                 quit()
     
-            #regenrate the map
+            #regenerate the map for debugging purposes
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_r:
                     mapMatrix, coloursList = mapGen()
@@ -242,7 +238,7 @@ def main():
         keys = pg.key.get_pressed()
         mainCam.update(keys, mouse)
 
-        #generate terrain
+        #generate the visible terrain
         verticelist = genTerrain(mapMatrix, coloursList, *mainCam.getXZ())
         for coords in verticelist:
             renderTriangle(*coords)
@@ -252,7 +248,8 @@ def main():
         except Exception: #divide by zero sometimes happens when a frame is rendered instantly
             pass
         text(0, 700, (1, 0, 0), str(round(timeTaken,1))+' FPS')
-        text(0, 800, (1, 0, 0), str(mainCam.getPos()))
+        text(0, 750, (1, 0, 0), str(mainCam.getPos()))
+        text(0, 800, (1, 0, 0), str(mainCam.getDir()))
 
         pg.display.flip() #update window with active buffer contents
         pg.time.wait(10) #wait a bit, avoids speed of simulation from being speed of execution of code
